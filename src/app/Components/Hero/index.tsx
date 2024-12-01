c76efea06da671d52cb833570cb8bf8f184294fc
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-12-01 10:45:37 -0700

    feat: add links to the hero buttons

--- src/app/Components/Hero/index.tsx
+++ src/app/Components/Hero/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import style from './style.module.css';
+import { Link } from 'react-scroll';
 
 export const Hero = ()=>{
     return <main className={`${style.hero}`}>
@@ -7,8 +8,8 @@ export const Hero = ()=>{
             <h1 className={`${style.hero__headline}`}>¡Lo que necesitas, <br />cuando lo necesitas!</h1>
             <p className={`${style.hero__caption}`}>Siempre a tu lado, listo para ofrecerte lo que necesitas, cuando lo necesitas. No más esperas, solo soluciones al instante</p>
             <div className={style.buttons}>
-                <button className={`${style.hero__button}`}>Ver servicios</button>
-                <button className={`${style.hero__button}`}>Contáctanos</button>
+                <button className={`${style.hero__button}`}><Link to='services' smooth spy>Ver servicios</Link></button>
+                <button className={`${style.hero__button}`}><a href="https://wa.me/526383805280?text=Hola. estoy interesado en su servicio." target='_blank'>Contáctanos</a></button>
             </div>
         </div>
         <div className={`${style["hero__headline-section--right"]}`} data-aos="fade-left">
